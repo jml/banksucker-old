eab1c3cba0dba925e8b3a61b2bde38fa670b88b5
Jonathan Lange <jml@mumak.net>
2012-12-30 16:28:26 +0000

    Initial import.

---
+++ NOTES.md
@@ -0,0 +1,34 @@
+
+## Lloyds transform
+
+1. Delete top row (headers)
+2. Delete C & D (account, sort)
+3. Delete F & G (running balance, ???)
+4. Extract credits for review
+   * Interest
+   * Transfers for purchasing big-spend savings
+   * Transfers for unexpected expenses
+5. Delete E (credits)
+6. Paste into Spending Plan
+
+
+## Amex transform
+
+1. Delete E, F
+2. Replace all entries in B with "AMEX"
+3. Extract credits
+4. Swap C & D
+5. Paste into Spending Plan
+
+
+## Spending plan update
+
+1. Reformat Date column (A) as date
+2. Reformat Value column (D) as pounds
+3. Sort by Value
+4. Sort by Date
+5. Delete any entries from before the period that are not in previous period
+6. Delete any duplicate entries
+7. Copy "Combined" and "Valid Category" helpers down to all new rows
+8. Delete any entries that are purchases from big spends or special expenses
+9. Fill in the field
---
+++ banksucker/lloyds.py
@@ -0,0 +1,52 @@
+from __future__ import print_function
+
+import sys
+
+from bs4 import BeautifulSoup
+import treq
+from twisted.internet.task import react
+
+
+LOGIN_FORM_URL = 'https://online.lloydstsb.co.uk/personal/logon/login.jsp'
+LOGIN_URL = 'https://online.lloydstsb.co.uk/personal/primarylogin'
+
+
+
+
+
+user_id_field = "frmLogin:strCustomerLogin_userID"
+password_field = "frmLogin:strCustomerLogin_pwd"
+
+remember_field = "frmLogin:loginRemember"
+'frmLogin:btnLogin1'
+
+hidden_data = {
+    'frmLogin': 'frmLogin',
+    'submitToken': '', # Need to get this from login page
+    'target': None,
+    'hdn_mobile': None,
+    }
+
+
+def chain(d, *callables):
+    for function in callables:
+        d.addCallback(function)
+    return d
+
+
+def post_done(response):
+    return treq.text_content(response)
+
+
+def parse_form(soup):
+    print(soup.form.find_all('input'))
+
+
+def main(reactor, *args):
+    d = treq.get(LOGIN_URL)
+    return chain(
+        d, post_done, BeautifulSoup,
+        parse_form)
+
+
+react(main, sys.argv)

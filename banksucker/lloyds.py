f6079b6cab1e53da53b96ede48d59c5fe0aa43f4
Jonathan Lange <jml@mumak.net>
2013-01-21 19:20:07 +0000

    Remove all the crap about the web for now.

--- banksucker/lloyds.py
+++ banksucker/lloyds.py
@@ -3,41 +3,6 @@ from __future__ import print_function
 import csv
 from datetime import datetime
 
-from bs4 import BeautifulSoup
-import treq
-
-
-LOGIN_FORM_URL = 'https://online.lloydstsb.co.uk/personal/logon/login.jsp'
-LOGIN_URL = 'https://online.lloydstsb.co.uk/personal/primarylogin'
-
-
-user_id_field = "frmLogin:strCustomerLogin_userID"
-password_field = "frmLogin:strCustomerLogin_pwd"
-
-remember_field = "frmLogin:loginRemember"
-'frmLogin:btnLogin1'
-
-hidden_data = {
-    'frmLogin': 'frmLogin',
-    'submitToken': '',  # Need to get this from login page
-    'target': None,
-    'hdn_mobile': None,
-    }
-
-
-def chain(d, *callables):
-    for function in callables:
-        d.addCallback(function)
-    return d
-
-
-def post_done(response):
-    return treq.text_content(response)
-
-
-def parse_form(soup):
-    return soup.form.find_all('input')
-
 
 def parse_row(row):
     return {
@@ -52,10 +17,3 @@ def parse_csv(csv_stream):
     reader = csv.reader(csv_stream)
     for row in reader:
         yield parse_row(row)
-
-
-def main(reactor, *args):
-    d = treq.get(LOGIN_URL)
-    return chain(
-        d, post_done, BeautifulSoup,
-        parse_form)
--- banksucker/tests/test_lloyds.py
+++ banksucker/tests/test_lloyds.py
@@ -1,55 +1,9 @@
-from bs4 import BeautifulSoup
-
 from datetime import date
 from StringIO import StringIO
 
 from testtools import TestCase
 
-from ..lloyds import parse_form, parse_row, parse_csv
-
-
-form = """
-<form id="frmLogin" name="frmLogin" method="post" action="https://online.lloydstsb.co.uk/personal/primarylogin" class="validationName:(login) validate:()" autocomplete="off" enctype="application/x-www-form-urlencoded">
-<div class="inner"><p><strong>Please enter&nbsp;your user ID and password below</strong></p>
-<div class="subPanel">
-<fieldset>
-<div class="formField validate:(required) validationName:(userID) clearfix">
-<div class="formFieldInner"><label for="frmLogin:strCustomerLogin_userID">User ID:</label>
-<input type="text" autocomplete="off" id="frmLogin:strCustomerLogin_userID" name="frmLogin:strCustomerLogin_userID" class="field setFocus" maxlength="30" value="723869582">
-</div>
-</div>
-<div class="formField validate:(required) validationName:(password) clearfix">
-<div class="formFieldInner"><label for="frmLogin:strCustomerLogin_pwd">Password:</label>
-<input type="password" id="frmLogin:strCustomerLogin_pwd" name="frmLogin:strCustomerLogin_pwd" class="field" maxlength="20" value=""></div>
-</div>
-<div class="formField fieldHelp checkbox clearfix">
-<div class="formFieldInner"><input id="frmLogin:loginRemember" type="checkbox" name="frmLogin:loginRemember" checked=""> <label for="frmLogin:loginRemember">Remember my User ID</label>
-<span class="cxtHelp"> <a class="cxtTrigger" title="Click to find out more about remembering your user ID." href="https://online.lloydstsb.co.uk/personal/logon/login.jsp#cxtHelp1">[?]</a>
-</span>
-<div id="cxtHelp1" class="help"><h3>Remember my user ID</h3>
-<p>Tick this box to save your user ID on this computer. This won't save your password though. You'll still have to enter it each time you want to access your account.</p></div>
-</div>
-</div>
-<div class="inner"><p><strong>Warning:</strong> Don't tick this box if you're using a public or shared computer</p></div>
-<div class="loginActions clearfix"><input id="frmLogin:btnLogin1" name="frmLogin:btnLogin1" type="image" class="submitAction" src="./login-form_files/continue-10-1329498072.png" alt="Continue" title="Continue">
-<ul id="frmLogin:pnlLogin2" class="linkList">
-<li><a id="frmLogin:lkLogin2" name="frmLogin:lkLogin2" href="https://online.lloydstsb.co.uk/personal/a/submitreplaceunlockauthmechanism/customeridentificationdata.jsp" title="Forgotten your password?">Forgotten your password?</a></li>
-<li><a id="frmLogin:lkLogin1" name="frmLogin:lkLogin1" href="https://online.lloydstsb.co.uk/personal/a/submitforgottenuserid/forgotUserID.jsp" title="Forgotten your User ID?">Forgotten your User ID?</a></li>
-<li><a id="frmLogin:lkLoginTrouble" name="frmLogin:lkLoginTrouble" href="https://online.lloydstsb.co.uk/personal/a/submitreplaceunlockauthmechanism/customeridentificationdata.jsp" title="Having problems logging in?">Having problems logging in?</a></li>
-</ul>
-</div>
-</fieldset>
-<input type="hidden" name="frmLogin" value="frmLogin"> <input type="hidden" name="submitToken" value="3839736"> <input type="hidden" name="target" value="">    <input type="hidden" name="hdn_mobile" value="">
-</div>
-</div>
-<input type="hidden" name="hasJS" value="true"></form>
-"""
-
-
-class TestLoginForm(TestCase):
-
-    def test_whatever(self):
-        parse_form(BeautifulSoup(form))
+from ..lloyds import parse_row, parse_csv
 
 
 class TestConversion(TestCase):
